a74602b7010b8148958cd52e7752b5621861fba7
SafetSrna <133990416+SafetSrna@users.noreply.github.com>
2023-06-19 10:44:00 +0200

    Add files via upload

---
+++ ponavljanje.py
@@ -0,0 +1,48 @@
+import random
+
+imena = ['Richard', 'Kevin', 'Edward', 'Debbie', 'Adam', 'Norma', 'Christopher', 'Karen', 'Tami', 'Michael', 'John',
+'Roseanna', 'Gerald', 'George', 'Vesta', 'Julie', 'Raymond', 'Janice', 'Susan', 'Kerry', 'Lorenzo', 'Holly', 'Dan',
+'Sherri', 'William', 'Karey', 'Marion', 'Melissa', 'Vincent', 'Ruby']
+
+prezimena = ['Arnold', 'Simmons', 'Velasco', 'Canada', 'Gibbs', 'Thompson', 'Rendall', 'Harris', 'Hendon', 'Lyles',
+'Perez', 'Cleary', 'Hoyman', 'Hall', 'Baker', 'Fichter', 'Colantuono', 'Moose', 'Howard', 'Davis', 'Nutt', 'Cornett',
+'Smith', 'Lemus', 'Beltran', 'Ho', 'Cook', 'Samuels', 'Rodriguez', 'Cano']
+
+radnici = []
+
+for _ in range(15):
+    ime = random.choice(imena)
+    prezime = random.choice(prezimena)
+    satnica = round(random.uniform(4, 6), 2)
+    radnik = {"ime": ime, "prezime": prezime, "satnica": satnica}
+    radnici.append(radnik)
+
+
+for radnik in radnici:
+    radnik["tjedni_sati"] = random.randint(20, 30)
+
+
+isplate = []
+ukupna_isplata = 0
+
+for radnik in radnici:
+    ime = radnik["ime"]
+    prezime = radnik["prezime"]
+    satnica = radnik["satnica"]
+    tjedni_sati = radnik["tjedni_sati"]
+    isplata = round(satnica * tjedni_sati, 2)
+    ukupna_isplata += isplata
+    isplate.append((ime, prezime, isplata))
+
+prosjecna_isplata = round(ukupna_isplata / len(radnici), 2)
+
+for ime, prezime, isplata in isplate:
+    print(f"Ime: {ime}\nPrezime: {prezime}\nIsplata: {isplata}\n")
+
+print(f"Ukupna isplata: {ukupna_isplata}")
+print(f"Prosjecna isplata: {prosjecna_isplata}")
+
+print("\nRadnici s isplatom iznad prosjeka:")
+for ime, prezime, isplata in isplate:
+    if isplata > prosjecna_isplata:
+        print(f"{ime} {prezime}")
